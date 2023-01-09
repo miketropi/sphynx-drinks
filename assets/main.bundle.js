ebae01a32ffb0d168e57caddf7d8d11a75bf3c35
Huynhhuynh <conghieuhuynh91@gmail.com>
2023-01-09 14:09:01 +0700

    revert base css

--- assets/main.bundle.js
+++ assets/main.bundle.js
@@ -248,7 +248,7 @@ function ProductCatCarousel() {
       }, {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1
         }
       }]
