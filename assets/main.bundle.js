9aa605d56752f60dd6d7d0c552e6d7a8026622bd
quocvietht03 <quocvietht03@gmail.com>
2023-01-13 13:55:56 +0700

    update menu and blog

--- assets/main.bundle.js
+++ assets/main.bundle.js
@@ -363,11 +363,19 @@ __webpack_require__.r(__webpack_exports__);
 function mobileMenu() {
   var mobileElems = document.querySelectorAll('.site-header__mobile');
   if (!mobileElems) return;
-  $('.site-header__mobile-toggle').on('click', function () {
+  $('.site-header__mobile-search-toggle').on('click', function () {
+    $(this).toggleClass('open');
+    $('.site-header__search-tools').slideToggle(250);
+  });
+  $('.site-header__mobile-menu-toggle').on('click', function () {
     $('.site-header__mobile').addClass('open');
   });
   $('.site-mobile__close').on('click', function () {
     $('.site-header__mobile').removeClass('open');
+  });
+  $('.menu-mobile-toggle-btn').on('click', function () {
+    $(this).toggleClass('open');
+    $(this).parent().children('ul').slideToggle(250);
   });
 }
 mobileMenu();
