41eff3b76c1d7d1dcd3ae0e1d985e29b9cf4bd1e
Huynhhuynh <conghieuhuynh91@gmail.com>
2023-02-03 16:12:20 +0700

    custom filter

--- assets/main.bundle.js
+++ assets/main.bundle.js
@@ -302,6 +302,37 @@ var updateMiniCartUi = function updateMiniCartUi(html) {
   });
 };
 
+var customFilterProduct = function customFilterProduct() {
+  $('.facets__main .js-filter:last-child .facets__selected').text('Popular');
+
+  // only show award winner & best sellers
+  $('.list-menu__item').each(function (index, value) {
+    var valueItem = $(this).find('input').val();
+    var idItem = $(this).find('input').attr('id');
+    if (idItem.indexOf("Filter-More") != -1) {
+      if (valueItem != 'Award Winners' && valueItem != 'Best Sellers') {
+        $(this).hide();
+      }
+    }
+  });
+
+  // Tonic and Soda & Mixers 
+  var ProductGridContainer = $('#ProductGridContainer');
+  var collectionName = ProductGridContainer.data('name-collection');
+  $('.js-filter').each(function (index, value) {
+    var filterItem = $(this).find('.facets__selected').text();
+    if (collectionName == 'Tonic and Soda' || collectionName == 'Non-Alcoholic Mixers') {
+      if (filterItem.indexOf("ABV") != -1 || filterItem.indexOf("Popular") != -1) {
+        $(this).hide();
+      }
+    }
+    if (filterItem.indexOf("Product type") != -1) {
+      console.log(filterItem);
+      $(this).find('.facets__selected').text('Type');
+    }
+  });
+};
+customFilterProduct();
 
 /***/ }),
 
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -14,7 +14,7 @@ const addToCart = async (quantity, variant_id) => {
       quantity,
       id: variant_id,
     },
-    error: function(XMLHttpRequest, textStatus) {
+    error: function (XMLHttpRequest, textStatus) {
       window.Shopify.onError(XMLHttpRequest, textStatus);
     }
   }
@@ -38,9 +38,46 @@ const updateMiniCartUi = (html) => {
   ];
 
   updateElems.forEach(item => {
-    if($(item.selector).length <= 0) return;
+    if ($(item.selector).length <= 0) return;
     $(item.selector).html($(html).find(item.selector).html());
   })
 }
 
 export { addToCart, updateMiniCartUi }
+
+const customFilterProduct = () => {
+  $('.facets__main .js-filter:last-child .facets__selected').text('Popular');
+
+  // only show award winner & best sellers
+  $('.list-menu__item').each(function (index, value) {
+    let valueItem = $(this).find('input').val();
+    let idItem = $(this).find('input').attr('id');
+    if (idItem.indexOf("Filter-More") != -1) {
+      if (valueItem != 'Award Winners' && valueItem != 'Best Sellers') {
+        $(this).hide();
+      }
+    }
+  });
+
+  // Tonic and Soda & Mixers 
+  const ProductGridContainer = $('#ProductGridContainer');
+  let collectionName = ProductGridContainer.data('name-collection');
+
+  $('.js-filter').each(function (index, value) {
+    let filterItem = $(this).find('.facets__selected').text();
+    if (collectionName == 'Tonic and Soda' || collectionName == 'Non-Alcoholic Mixers') {
+      if (filterItem.indexOf("ABV") != -1 || filterItem.indexOf("Popular") != -1) {
+        $(this).hide();
+      }
+    }
+
+    if (filterItem.indexOf("Product type") != -1) {
+      console.log(filterItem)
+      $(this).find('.facets__selected').text('Type')
+    }
+
+  });
+
+}
+
+customFilterProduct();
